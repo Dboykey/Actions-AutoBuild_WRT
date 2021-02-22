ab7c31ad8134e560c38c8eea10eba5d112b32eae
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-02-22 19:00:08 +0800

    Update diy-CK-part2.sh

--- diy-CK-part2.sh
+++ diy-CK-part2.sh
@@ -24,3 +24,17 @@ sed -i 's/高级重启/关机/' feeds/luci/applications/luci-app-advanced-reboot
 # feeds/luci/applications/luci-app-diag-core/luasrc/controller/luci_diag.lua
 # 删除不需要的部分
 sed -i '9,12d' feeds/luci/applications/luci-app-diag-core/luasrc/controller/luci_diag.lua
+
+rm -rf feeds/helloworld/naiveproxy
+rm -rf feeds/helloworld/tcping
+rm -rf feeds/helloworld/xray-core
+rm -rf package/lean/dns2socks
+rm -rf package/lean/ipt2socks
+rm -rf package/lean/kcptun
+rm -rf package/lean/microsocks
+rm -rf package/lean/pdnsd-alt
+rm -rf package/lean/shadowsocksr-libev
+rm -rf package/lean/simple-obfs
+rm -rf package/lean/trojan
+rm -rf package/lean/v2ray-plugin
+rm -rf package/lean/xray-plugin
