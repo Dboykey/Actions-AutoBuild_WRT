42460ffafad4b72b05540f012eb6cee1c5306aba
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-02-23 09:29:28 +0000

    Add files via upload

---
+++ diy-2100-p1.sh
@@ -0,0 +1,42 @@
+mv feeds.conf.default feeds.conf.default.bak
+touch feeds.conf.default
+echo "src-git packages https://github.com/coolsnowwolf/packages;openwrt-19.07" >>feeds.conf.default
+echo "src-git luci https://github.com/coolsnowwolf/luci" >>feeds.conf.default
+echo "src-git routing https://git.openwrt.org/feed/routing.git;openwrt-19.07" >>feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld" >>feeds.conf.default
+echo "src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main" >>feeds.conf.default
+echo "src-git adockerman https://github.com/lisaac/luci-app-dockerman" >>feeds.conf.default
+echo "src-git ldockerman https://github.com/lisaac/luci-lib-docker" >>feeds.conf.default
+
+# 添加Rosy
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
+
+# 添加vssr
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# 添加 OpenCash
+git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+
+# 添加adguardhome、smartdns
+git clone https://github.com/Dboykey/CKdiy.git package/CKdiy
+rm -rf feeds/packages/admin/ipmitool
+
+# package/lean/default-settings/files/zzz-default-settings
+##找到字符串“uci commit luci”后，在前面增加一行
+#sed -i '/uci commit luci/i\\uci set luci.main.mediaurlbase=/luci-static/rosy' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit luci/i\\uci set luci.main.mediaurlbase=/luci-static/argon' package/lean/default-settings/files/zzz-default-settings
+##找到字符串“shadow”后，删除一行
+sed -i -e '/shadow/d' package/lean/default-settings/files/zzz-default-settings
+##找到字符串“uci commit luci”后，在后面增加一行，又一行
+sed -i "/uci commit luci/a\\uci commit network" package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit luci/a\\uci set network.lan.netmask='255.255.255.0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit luci/a\\uci set network.lan.ipaddr='192.168.2.1'" package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit luci/a\\ " package/lean/default-settings/files/zzz-default-settings
+
+# Openwrt固件的超频1100Mhz设置
+#target/linux/ramips/patches-4.14/102-mt7621-fix-cpu-clk-add-clkdev.patch
+rm target/linux/ramips/patches-4.14/102-mt7621-fix-cpu-clk-add-clkdev.patch
+cp package/CKdiy/AC2100/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-4.14/
---
+++ diy-2100-p2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# package/lean/default-settings/i18n/more.zh-cn.po
+# 把“双分区启动切换”替换为“关机”
+#sed -i 's/双分区启动切换/关机/' package/lean/default-settings/i18n/more.zh-cn.po
+
+# feeds/luci/applications/luci-app-advanced-reboot/po/zh-cn/advanced-reboot.po
+# 把“高级重启”替换为“关机”
+sed -i 's/高级重启/关机/' feeds/luci/applications/luci-app-advanced-reboot/po/zh-cn/advanced-reboot.po
+
+# feeds/luci/applications/luci-app-diag-core/luasrc/controller/luci_diag.lua
+# 删除不需要的部分
+sed -i '9,12d' feeds/luci/applications/luci-app-diag-core/luasrc/controller/luci_diag.lua
+
+rm -rf feeds/helloworld/naiveproxy
+rm -rf feeds/helloworld/tcping
+rm -rf feeds/helloworld/xray-core
+ln -s feeds/diy1/naiveproxy feeds/helloworld/naiveproxy
+ln -s feeds/diy1/tcping feeds/helloworld/tcping
+ln -s feeds/diy1/xray-core feeds/helloworld/xray-core
+rm -rf package/lean/dns2socks
+rm -rf package/lean/ipt2socks
+rm -rf package/lean/kcptun
+rm -rf package/lean/microsocks
+rm -rf package/lean/pdnsd-alt
+rm -rf package/lean/shadowsocksr-libev
+rm -rf package/lean/simple-obfs
+rm -rf package/lean/trojan
+rm -rf package/lean/v2ray-plugin
+rm -rf package/lean/xray-plugin
